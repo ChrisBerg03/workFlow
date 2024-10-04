aff82512eae431c32d12738d2efe08af29ebe48a
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-10-04 10:48:58 +0200

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+const counter = document.getElementById("counter");
+const counterBtn = document.getElementById("counterBtn");
+
+let count = 0;
+counterBtn.addEventListener("click", () => {
+    if (count <= 10) {
+        count++;
+        counter.innerText = `${count}`;
+    } else {
+        counter.innerHTML = "";
+        counter.innerHTML = `<img id="something" src="/public/scaryThing.png"/>`;
+    }
+});
